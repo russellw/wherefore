1f80aba240e5897f76d44af6f558475e3eee0e4a
Russell Wallace <russell.wallace@gmail.com>
2024-04-10 15:53:07 +0100

    args

---
+++ wherefore.py
@@ -0,0 +1,19 @@
+import argparse
+import subprocess
+
+from openai import OpenAI
+
+parser = argparse.ArgumentParser(description="Explain the meaning of a symbol in code")
+parser.add_argument("name", type=str, help="The symbol to search for")
+parser.add_argument(
+    "--searcher",
+    type=str,
+    default="rg",
+    help="The searcher tool to use (default: 'rg')",
+)
+args = parser.parse_args()
+name = args.name
+
+command = [args.searcher, "-C", "10", args.name]
+result = subprocess.run(command, capture_output=True, text=True, check=True)
+print("ripgrep output:\n", result.stdout)
